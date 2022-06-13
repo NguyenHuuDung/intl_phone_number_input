438d779a0476294465e717a109b78d72f8a82b26
Nguyễn Hữu Dũng <>
2022-06-13 16:42:41 +0700

    create

---
+++ intl_phone_number_input-develop/example/lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var darkTheme = ThemeData.dark().copyWith(primaryColor: Colors.blue);
+
+    return MaterialApp(
+      title: 'Demo',
+      themeMode: ThemeMode.light,
+      darkTheme: darkTheme,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(title: Text('Demo')),
+          body: MyHomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'NG';
+  PhoneNumber number = PhoneNumber(isoCode: 'NG');
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                height: 40,
+                // color: Colors.yellow,
+                padding: EdgeInsets.only(left: 31, right: 12),
+                child: InternationalPhoneNumberInput(
+                  onInputChanged: (PhoneNumber number) {
+                    print(number.phoneNumber);
+                  },
+                  onInputValidated: (bool value) {
+                    print(value);
+                  },
+                  selectorConfig: SelectorConfig(
+                    selectorType: PhoneInputSelectorType.DIALOG,
+                  ),
+                  ignoreBlank: true,
+                  autoValidateMode: AutovalidateMode.disabled,
+                  selectorTextStyle: TextStyle(color: Colors.black),
+                  initialValue: number,
+                  textFieldController: controller,
+                  formatInput: false,
+                  keyboardType: TextInputType.numberWithOptions(
+                      signed: true, decimal: true),
+                  inputBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  onSaved: (PhoneNumber number) {
+                    print('On Saved: $number');
+                  },
+                  boxDecoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      border: Border.all(color: Colors.blueAccent)),
+                )),
+            ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.validate();
+              },
+              child: Text('Validate'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                getPhoneNumber('+15417543010');
+              },
+              child: Text('Update'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.save();
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getPhoneNumber(String phoneNumber) async {
+    PhoneNumber number =
+        await PhoneNumber.getRegionInfoFromPhoneNumber(phoneNumber, 'US');
+
+    setState(() {
+      this.number = number;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
